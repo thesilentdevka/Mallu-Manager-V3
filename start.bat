c223b79972bff2d302a22c7e12d8c03f733fb463
DEVAKRISHNA PRASOBH <devakrishnaprasobh@gmail.com>
2021-11-26 13:08:05 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
 TITLE Emilia Robot
 :: Enables virtual env mode and then starts Emilia
-env\scripts\activate.bat && py -m EmiliaAnimeBot
+env\scripts\activate.bat && py -m mallu-manager-v3
